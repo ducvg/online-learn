12161df7449ee9c1fa77104e0613badb0b27ab90
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2023-07-11 20:06:59 +0700

    delete code

--- code/adminator220623/src/main/resources/static/js/about.js
+++
@@ -1,9 +0,0 @@
-var navbar = document.getElementById('web-navbar')
-
-window.onscroll = function () {
-    if (window.pageYOffset > 100) {
-        navbar.style.background = "#555555";
-    } else {
-        navbar.style.background = "transparent";
-    }
-}
