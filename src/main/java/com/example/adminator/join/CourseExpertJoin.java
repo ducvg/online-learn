affcfe4b96dd5aa6de471a31950b234d004ee39b
duchai2501 <99119719+duchai2501@users.noreply.github.com>
2023-07-15 16:14:34 +0700

    Update ExpectController
    funtion relate to AssignExpect

---
+++ src/main/java/com/example/adminator/join/CourseExpertJoin.java
@@ -0,0 +1,21 @@
+package com.example.adminator.join;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CourseExpertJoin {
+    private String Name;
+    private String Email;
+    private String Title;
+    private String Status;
+    private int nigga;
+
+    public CourseExpertJoin(String name, String email, String title, String status) {
+        Name = name;
+        Email = email;
+        Title = title;
+        Status = status;
+    }
+}
